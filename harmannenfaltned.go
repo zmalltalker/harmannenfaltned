e81a332cc1308cb4cdf5a353cbaec1eccd1c37c3
Marius Mathiesen <marius@shortcut.no>
2014-10-30 13:39:17 +0100

    Exit with proper status codes, allow q flag
    The program will now exit with a non-zero exit code in the unlikely
    event that the mountain did in fact fall. Also, allow running with a -q
    switch to not show any output, only exit with proper exit code.

--- harmannenfaltned.go
+++ harmannenfaltned.go
@@ -5,29 +5,53 @@ import (
 	"encoding/json"
 	"net/http"
 	"io/ioutil"
+	"errors"
+	"os"
+	"flag"
 )
 
 type Mannen struct {
 	FaltNed bool `json:"falt_ned"`
 }
 
-func main(){
+func getResponse() (error, bool) {
 	resp, err := http.Get("http://www.vondess.com/mannen/api")
 	defer resp.Body.Close()
 	if err == nil {
 		body, ioerr := ioutil.ReadAll(resp.Body)
 		if ioerr != nil {
-			panic("IO error")
+			return errors.New("IO error"), false
 		}
 		var mannen Mannen
 		jsonerr := json.Unmarshal(body, &mannen)
 		if jsonerr != nil {
-			panic("OMG")
+			return errors.New("OMG"), false
 		}
-		if mannen.FaltNed {
-			fmt.Println("Ja")
+		return nil, mannen.FaltNed
+	} else {
+		return errors.New("API error"), false
+	}
+}
+func main(){
+	runQuiet := flag.Bool("q", false, "Run quietly")
+	flag.Parse()
+	err, v := getResponse()
+	if err == nil {
+		if v {
+			if !*runQuiet {
+				fmt.Println("Ja")
+			}
+			os.Exit(1)
 		} else {
-			fmt.Println("Nei")
+			if !*runQuiet {
+				fmt.Println("Nei")
+			}
+			os.Exit(0)
 		}
+	} else {
+		if !*runQuiet {
+			fmt.Printf("Error: %v\n", err)
+		}
+		os.Exit(2)
 	}
 }
